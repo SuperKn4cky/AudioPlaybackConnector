db07d162409d6d882e10f9e55f969f7c91782d3a
Quentin <superretards22@gmail.com>
2026-02-21 16:40:57 +0100

    settings: store config in %APPDATA% and write pretty-printed JSON
    - move settings file from executable directory to %APPDATA%/AudioPlaybackConnector/AudioPlaybackConnector.json\n- create target directory automatically when saving settings\n- keep backward compatibility by loading legacy local config if AppData config does not exist\n- serialize settings with human-readable pretty formatting

--- SettingsUtil.hpp
+++ SettingsUtil.hpp
@@ -1,8 +1,81 @@
 #pragma once
 
 constexpr auto CONFIG_NAME = L"AudioPlaybackConnector.json";
+constexpr auto APPDATA_DIR_NAME = L"AudioPlaybackConnector";
 constexpr auto BUFFER_SIZE = 4096;
 constexpr size_t MAX_CONFIG_SIZE = 1024 * 1024;
+
+fs::path GetLegacySettingsPath()
+{
+	return GetModuleFsPath(g_hInst).remove_filename() / CONFIG_NAME;
+}
+
+fs::path GetSettingsPath()
+{
+	const wchar_t* appData = _wgetenv(L"APPDATA");
+	if (appData && appData[0] != L'\0')
+	{
+		return fs::path(appData) / APPDATA_DIR_NAME / CONFIG_NAME;
+	}
+
+	return GetLegacySettingsPath();
+}
+
+fs::path ResolveSettingsLoadPath()
+{
+	const auto preferredPath = GetSettingsPath();
+	std::error_code ec;
+	if (fs::exists(preferredPath, ec) && !ec)
+	{
+		return preferredPath;
+	}
+
+	const auto legacyPath = GetLegacySettingsPath();
+	ec.clear();
+	if (fs::exists(legacyPath, ec) && !ec)
+	{
+		return legacyPath;
+	}
+
+	return preferredPath;
+}
+
+std::wstring BuildPrettySettingsJson(const std::vector<std::wstring>& lastDeviceIds)
+{
+	std::wstring json;
+	json.reserve(256 + lastDeviceIds.size() * 64);
+	json += L"{\n";
+	json += L"  \"reconnect\": ";
+	json += g_reconnect ? L"true" : L"false";
+	json += L",\n";
+	json += L"  \"showNotification\": ";
+	json += g_showNotification ? L"true" : L"false";
+	json += L",\n";
+	json += L"  \"lastDevices\": [";
+	if (!lastDeviceIds.empty())
+	{
+		json += L"\n";
+	}
+
+	for (size_t i = 0; i < lastDeviceIds.size(); ++i)
+	{
+		json += L"    ";
+		json += std::wstring(JsonValue::CreateStringValue(lastDeviceIds[i]).Stringify());
+		if (i + 1 < lastDeviceIds.size())
+		{
+			json += L",";
+		}
+		json += L"\n";
+	}
+
+	if (!lastDeviceIds.empty())
+	{
+		json += L"  ";
+	}
+	json += L"]\n";
+	json += L"}\n";
+	return json;
+}
 
 void DefaultSettings()
 {
@@ -17,7 +90,8 @@ void LoadSettings()
 	{
 		DefaultSettings();
 
-		wil::unique_hfile hFile(CreateFileW((GetModuleFsPath(g_hInst).remove_filename() / CONFIG_NAME).c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr));
+		const auto settingsPath = ResolveSettingsLoadPath();
+		wil::unique_hfile hFile(CreateFileW(settingsPath.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr));
 		THROW_LAST_ERROR_IF(!hFile);
 
 		std::string string;
@@ -56,24 +130,29 @@ void SaveSettings()
 {
 	try
 	{
-		JsonObject jsonObj;
-		jsonObj.Insert(L"reconnect", JsonValue::CreateBooleanValue(g_reconnect));
-		jsonObj.Insert(L"showNotification", JsonValue::CreateBooleanValue(g_showNotification));
-
-		JsonArray lastDevices;
+		std::vector<std::wstring> lastDeviceIds;
 		{
 			std::scoped_lock lock(g_audioPlaybackConnectionsMutex);
+			lastDeviceIds.reserve(g_audioPlaybackConnections.size());
 			for (const auto& i : g_audioPlaybackConnections)
 			{
-				lastDevices.Append(JsonValue::CreateStringValue(i.first));
+				lastDeviceIds.push_back(i.first);
 			}
 		}
-		jsonObj.Insert(L"lastDevices", lastDevices);
 
-		wil::unique_hfile hFile(CreateFileW((GetModuleFsPath(g_hInst).remove_filename() / CONFIG_NAME).c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr));
+		const auto settingsPath = GetSettingsPath();
+		const auto settingsDirectory = settingsPath.parent_path();
+		if (!settingsDirectory.empty())
+		{
+			std::error_code ec;
+			fs::create_directories(settingsDirectory, ec);
+			THROW_HR_IF(E_FAIL, ec);
+		}
+
+		wil::unique_hfile hFile(CreateFileW(settingsPath.c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr));
 		THROW_LAST_ERROR_IF(!hFile);
 
-		std::string utf8 = Utf16ToUtf8(jsonObj.Stringify());
+		std::string utf8 = Utf16ToUtf8(BuildPrettySettingsJson(lastDeviceIds));
 		DWORD written = 0;
 		THROW_IF_WIN32_BOOL_FALSE(WriteFile(hFile.get(), utf8.data(), static_cast<DWORD>(utf8.size()), &written, nullptr));
 		THROW_HR_IF(E_FAIL, written != utf8.size());
